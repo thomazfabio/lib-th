9b3758a8988225625e66b7fd736c9aa930aad5ba
fabio thomaz <fabio_thomaz@live.com>
2024-05-11 21:04:57 -0300

    adicionado componente valida cep

---
+++ src/components/ValidaCep.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="cep">CEP:</label>
+          <input
+            id="cep"
+            v-model="cep"
+            placeholder="00000-000"
+            maxlength="9"
+          >
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        cep: ''
+      };
+    },
+    computed: {
+      formattedCep() {
+        let value = this.cep.replace(/\D/g, '');
+        return value.replace(/^(\d{5})(\d{3})$/, '$1-$2');
+      }
+    },
+    watch: {
+      formattedCep(newVal) {
+        this.cep = newVal;
+      }
+    },
+    methods: {
+      submitForm() {
+        // Aqui você pode enviar o CEP para a API ou fazer o que desejar
+        console.log('CEP enviado:', this.cep);
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .form-group {
+    margin-bottom: 20px;
+  }
+  </style>
+  
--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,12 +1,16 @@
 <script setup>
 import ValidaCpf from '../components/ValidaCpf.vue'
 import ValidaCnpj from '../components/ValidaCnpj.vue'
+import ValidaCep from '../components/ValidaCep.vue'
 </script>
 
 <template>
     <h1>Alguns componentes aqui</h1>
+    <hr>
     <ValidaCpf />
     <hr>
     <ValidaCnpj />
+    <hr>
+    <ValidaCep />
 
 </template>
